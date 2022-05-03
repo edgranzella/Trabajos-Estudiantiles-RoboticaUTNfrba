3e5f8e771d4fce1285f3933d28fe54c36d34be5a
edgranzella <dgranzella@hotmail.com>
2022-05-03 14:38:00 -0300

    Delete TP2 Fernando Resquin directory

--- TP2 Fernando Resquin/Matlab/Plataforma/P_Cin_Dir.m
+++
@@ -1,5 +0,0 @@
-function D = P_Cin_Dir(Wi, Wd, q , r, b)
-A=[(r*cos(q))/2 (r*cos(q))/2 ; (r*sin(q))/2 (r*sin(q))/2 ; -r/(2*b) r/(2*b)];
-D = A*[Wi ; Wd];                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-
-    
--- TP2 Fernando Resquin/Matlab/Plataforma/P_Cin_Inv.m
+++
@@ -1,4 +0,0 @@
-function D = P_Cin_Inv(Vx, Vy, W, q, r, b)
-B=[cos(q)/r  sin(q)/r  -b/r ; cos(q)/r sin(q)/r b/r];
-D = B*[Vx ; Vy ; W];
-    
